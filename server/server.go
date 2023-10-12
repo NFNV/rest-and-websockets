24dfd65d7363fbdabf389e1e0e7476a6acf136a3
Nicolás Valiconto <nvaliconto@outlook.com>
2023-10-12 19:45:18 -0300

    server and main are done

--- server/server.go
+++ server/server.go
@@ -9,39 +9,41 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Config struct holds the configuration parameters for the server.
 type Config struct {
-	Port string
-	JWTSecret string
+	Port        string
+	JWTSecret   string
 	DatabaseUrl string
 }
 
+// Server is an interface that defines a method to retrieve the server's configuration.
 type Server interface {
 	Config() *Config
 }
 
+// Broker struct represents the server instance.
 type Broker struct {
 	config *Config
 	router *mux.Router
 }
 
+// Config returns the configuration of the server.
 func (b *Broker) Config() *Config {
 	return b.config
 }
 
-func NewServer(ctx context.Context,  config *Config) (*Broker, error) {
+// NewServer creates a new server instance based on the provided configuration.
+func NewServer(ctx context.Context, config *Config) (*Broker, error) {
 	if config.Port == "" {
 		return nil, errors.New("port is required")
-
 	}
 
 	if config.JWTSecret == "" {
 		return nil, errors.New("secret is required")
-
 	}
 
 	if config.DatabaseUrl == "" {
 		return nil, errors.New("database is required")
-
 	}
 
 	broker := &Broker{
@@ -51,8 +53,11 @@ func NewServer(ctx context.Context,  config *Config) (*Broker, error) {
 	return broker, nil
 }
 
-func (b *Broker) Start(binder func (s Server, r *mux.Router)) {
+// Start initiates the server by binding routes to the router and listening for incoming requests.
+func (b *Broker) Start(binder func(s Server, r *mux.Router)) {
+	// Create a new router for this server instance.
 	b.router = mux.NewRouter()
+	// Bind routes to the router.
 	binder(b, b.router)
 	log.Println("Starting server on", b.Config().Port)
 	if err := http.ListenAndServe(b.config.Port, b.router); err != nil {
